d08ebf6692a59f5aac35790c45acf132992a55a7
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2022-05-07 22:00:54 +0500

    Create Login System Project

--- django_login/django_login/urls.py
+++ django_login/django_login/urls.py
@@ -1,4 +1,4 @@
-"""cms URL Configuration
+"""django_login URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
